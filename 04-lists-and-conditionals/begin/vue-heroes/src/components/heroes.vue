bd9806475374f8fa6285050dd5e2c58cdcf2645d
willsmith <will.smith@fieldagent.net>
2021-07-15 16:31:46 -0500

    Vue if-conditional basic example

--- 04-lists-and-conditionals/begin/vue-heroes/src/components/heroes.vue
+++ 04-lists-and-conditionals/begin/vue-heroes/src/components/heroes.vue
@@ -10,12 +10,12 @@
         </header>
         <ul class="list is-hoverable">
           <li v-for="hero in heroes" :key="hero.id">
-            <a class="list-item"><span>{{ hero.firstName }}</span></a>
+            <a class="list-item" @click="selectedHero = hero" :class="{'is-active': selectedHero == hero}"><span>{{ hero.firstName }}</span></a>
           </li>
         </ul>
       </div>
     </div>
-    <div class="columns">
+    <div class="columns" v-if="selectedHero">
       <div class="column is-3">
         <header class="card-header">
           <p class="card-header-title">{{ selectedHero.firstName }}</p>
@@ -64,12 +64,7 @@ export default {
   name: 'Heroes',
   data() {
     return {
-      selectedHero: {
-        id: 111,
-        firstName: '...',
-        lastName: '...',
-        description: '...',
-      },
+      selectedHero: undefined,
       heroes: [
         {
           id: 10,

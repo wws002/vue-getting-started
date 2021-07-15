607a3f47c0ee196844453bbcd915dd044ae56a49
willsmith <will.smith@fieldagent.net>
2021-07-15 14:07:22 -0500

    key-binding basics

--- 03-data-and-events/begin/vue-heroes/src/components/heroes.vue
+++ 03-data-and-events/begin/vue-heroes/src/components/heroes.vue
@@ -47,7 +47,7 @@
                 <label for="power">
                   super power
                   <div class="select is-primary">
-                    <select id="power">
+                    <select id="power" v-model="hero.power" :class="{ invalid: !hero.power }" @keyup.esc="clearPower">
                       <option disabled value>Please select one</option>
                       <option>Speed</option>
                       <option>Flight</option>
@@ -102,6 +102,9 @@ export default {
   methods: {
     cancelHero() {
       this.message = '';
+    },
+    clearPower() {
+      this.hero.power = '';
     },
     saveHero() {
       this.message = JSON.stringify(this.hero, null, '\n');

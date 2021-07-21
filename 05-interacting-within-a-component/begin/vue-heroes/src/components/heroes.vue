6fed5ee130da9d57b72bf716040eb0aec31f119d
willsmith <will.smith@fieldagent.net>
2021-07-21 15:56:49 -0500

    basic component life cycles

--- 05-interacting-within-a-component/begin/vue-heroes/src/components/heroes.vue
+++ 05-interacting-within-a-component/begin/vue-heroes/src/components/heroes.vue
@@ -113,7 +113,7 @@ export default {
   name: 'Heroes',
   data() {
     return {
-      heroes: ourHeroes,
+      heroes: [],
       selectedHero: undefined,
       message: '',
     };
@@ -123,7 +123,21 @@ export default {
       return `${this.selectedHero.firstName} ${this.selectedHero.lastName}`;
     },
   },
+  created() {
+    this.loadHeroes();
+  },
   methods: {
+    async getHeroes() {
+      return new Promise(resolve => {
+        setTimeout(() => resolve(ourHeroes), 1500);
+      });
+    },
+    async loadHeroes() {
+      this.heroes = [];
+      this.message = 'getting the heroes. one second please';
+      this.heroes = await this.getHeroes();
+      this.message = '';
+    },
     handleTheCapes(newValue) {
       const value = parseInt(newValue, 10);
       switch (value) {

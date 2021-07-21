0411f05fecfb62b97279d195d8cf2782fc6394dc
willsmith <will.smith@fieldagent.net>
2021-07-21 16:14:49 -0500

    vue watcher example

--- 05-interacting-within-a-component/begin/vue-heroes/src/components/heroes.vue
+++ 05-interacting-within-a-component/begin/vue-heroes/src/components/heroes.vue
@@ -61,6 +61,19 @@
                   v-model="selectedHero.description"
                 />
               </div>
+              <div class="field">
+                <label class="label" for="capeCounter">cape counter</label>
+                <input
+                  class="input"
+                  id="capeCounter"
+                  type="number"
+                  v-model="selectedHero.capeCounter"
+                />
+              </div>
+              <div class="field">
+                <label class="label" for="capeMessage">cape message</label>
+                <label class="input" name="capeMessage">{{ capeMessage }}</label>
+              </div>
             </div>
           </div>
           <footer class="card-footer">
@@ -88,24 +101,28 @@ const ourHeroes = [
     id: 10,
     firstName: 'Ella',
     lastName: 'Papa',
+    capeCounter: 1,
     description: 'fashionista',
   },
   {
     id: 20,
     firstName: 'Madelyn',
     lastName: 'Papa',
+    capeCounter: 3,
     description: 'the cat whisperer',
   },
   {
     id: 30,
     firstName: 'Haley',
     lastName: 'Papa',
+    capeCounter: 2,
     description: 'pen wielder',
   },
   {
     id: 40,
     firstName: 'Landon',
     lastName: 'Papa',
+    capeCounter: 0,
     description: 'arc trooper',
   },
 ];
@@ -116,6 +133,7 @@ export default {
       heroes: [],
       selectedHero: undefined,
       message: '',
+      capeMessage: '',
     };
   },
   computed: {
@@ -165,6 +183,15 @@ export default {
     },
     selectHero(hero) {
       this.selectedHero = hero;
+    },
+  },
+  watch: {
+    'selectedHero.capeCounter': {
+      immediate: true,
+      handler(newValue, oldValue) {
+        console.log(`Watcher evaluated. old=${oldValue}, new=${newValue}`);
+        this.handleTheCapes(newValue);
+      },
     },
   },
 };
